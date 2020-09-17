d2f13ad9d84d0531af1aa9d76cf207bb65cbfeac
Fares84 <[2528@holbertonschool.com]>
2020-09-17 21:47:46 +0000

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - function
+ * full alphabet
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}

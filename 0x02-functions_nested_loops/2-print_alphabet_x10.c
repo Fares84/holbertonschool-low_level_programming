dea020d4db9e61f431cc8e19262b7007d833bf30
Fares84 <[2528@holbertonschool.com]>
2020-09-22 19:56:19 +0000

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_alphabet_10_times - prints 10 times the alphabet in lowercase
+ *
+ *Return: Always 0
+ */
+
+void print_alphabet_x10(void);
+
+{
+	char ch;
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}

461d5e574874c41b3883b8d533ff024926dc2552
Fares84 <[2528@holbertonschool.com]>
2020-09-22 20:00:16 +0000

    print sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ *print_sign - function that display the sign with absolute value 
+ *@n: int
+ *Return: 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	return (0);
+}

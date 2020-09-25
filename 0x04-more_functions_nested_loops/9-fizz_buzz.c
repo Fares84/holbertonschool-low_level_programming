167fd73f34572826593dc003acef76c3ae678b58
Fares84 <[2528@holbertonschool.com]>
2020-09-25 11:41:53 +0000

    updated tasks

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,1 +1,26 @@
-#include "holberton.h"
+#include <stdio.h>
+/**
+ * main -  prints the numbers multiples of 3 and 5
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i < 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}

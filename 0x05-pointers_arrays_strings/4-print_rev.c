be4ff5f8fc89a9c0e8c58ea8f4ce8a517006f104
Fares84 <[2528@holbertonschool.com]>
2020-09-29 21:53:22 +0000

    print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *print_rev - function that prints a string in reverse
+ *@s : int
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	while (i - 1 >= 0)
+	{
+		_putchar(s[i - 1]);
+		i--;
+	}
+	_putchar('\n');
+}

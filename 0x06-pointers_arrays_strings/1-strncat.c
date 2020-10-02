e7ed51c10ab6f629465a92ca98a6ae0b0c679961
Fares84 <[2528@holbertonschool.com]>
2020-10-02 21:12:28 +0000

    concatenates two strings and add byte

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -21,8 +21,9 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (j = 0 ; j < n && src[j] != '\0' ; j++)
 	{
-		dest[i] = src[j];
+		dest[i + j] = src[j];
 	}
-	dest[i] = '\0';
+
+	dest[i + j] = '\0';
 	return (dest);
 }

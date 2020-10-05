a8a8d5280b91ad5e5931fb2e18b4d48f8a2a7b7c
Fares84 <[2528@holbertonschool.com]>
2020-10-05 21:53:43 +0000

    length string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *_strchr - function that locates a character in a string
+ * @s: buffer
+ * @c: char
+ * Return: s
+ **/
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != 'c'; i++)
+	{
+		if (s[i] = 'c')
+		    break;
+	}
+		return (s);
+}

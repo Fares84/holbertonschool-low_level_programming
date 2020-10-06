45447968d1b221d0138cd73f3b2090e430c2eb60
Fares84 <[2528@holbertonschool.com]>
2020-10-06 12:57:51 +0000

    print chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,21 +1,21 @@
 #include "holberton.h"
 
 /**
- * print_chessboard - function that prints the sum of the two diagonals of a squ
- *are matrix of integers
+ * print_chessboard - function that prints the sum of the two diagonals of a
+ *square matrix of integers
  * @a: cheese
  * Return: Nothing.
  */
 void print_chessboard(char (*a)[8])
 {
-        int i, j;
+	int i, j;
 
-        for (i = 0; i < 8; i++)
-        {
-                for (j = 0; j < 8; j++)
-                {
-                        _putchar (a[i][j]);
-                }
-                _putchar('\n');
-        }
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar (a[i][j]);
+		}
+		_putchar('\n');
+	}
 }

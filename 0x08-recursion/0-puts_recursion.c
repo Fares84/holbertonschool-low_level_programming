690f6a2c659c31db154d82c06d99054b26b3b28f
Fares84 <[2528@holbertonschool.com]>
2020-10-07 21:43:03 +0000

    prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - function that prints a string
+ *@s: char
+ *Return: NULL
+ **/
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+}

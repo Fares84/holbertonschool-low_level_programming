9b09e537917db8683a07f806948bf8e89633b26a
Fares84 <[2528@holbertonschool.com]>
2020-10-11 00:44:45 +0000

    argv[argc]

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - program that prints its name
+ *@argc: variable of the arguments
+ *@argv: an array containing the program arguments
+ *Return: 0 on success
+ **/
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	{
+		printf("%s\n", argv[argc]);
+	}
+	return (0);
+}

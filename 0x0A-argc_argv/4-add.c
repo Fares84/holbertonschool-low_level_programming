da1c7c0a4d9cbbbdec67fd5a82f30605e58fda7c
Fares84 <[2528@holbertonschool.com]>
2020-10-12 13:51:44 +0000

    multi numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,11 +10,11 @@
  **/
 int main(int argc, char *argv[])
 {
-		int i, j, sum=0;
+		int i, j, sum = 0;
 
 		if (argc > 0)
 		{
-			for (i = 1; i <argc; i++)
+			for (i = 1; i < argc; i++)
 			{
 				for (j = 0; argv[i][j] != '\0'; j++)
 				{
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
 				sum = (sum + atoi(argv[i]));
 			}
 					printf("%d\n", sum);
-	       }
+		}
 		else
 		printf("0\n");
 		return (0);

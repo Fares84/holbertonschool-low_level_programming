dcc0ff1bf2c1da674952f01c79e96ecca39f994b
Fares84 <[2528@holbertonschool.com]>
2020-10-13 21:54:30 +0000

    string up

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ **_strdup - function that concatenates two strings
+ * space in memory
+ *@str: string to copy
+ *Return: char
+ **/
+char *str_concat(char *s1, char *s2)
+{
+	
+}

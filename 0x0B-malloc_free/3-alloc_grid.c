5a0d9bda85b3332844310314680a7930d761e211
Fares84 <[2528@holbertonschool.com]>
2020-10-13 21:56:19 +0000

    return free

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+***alloc_grid - function function that returns a pointer to a newly allocated
+*@width: width of array
+* @height: height of array
+* Return: a double pointer to the 2D array
+**/
+int **alloc_grid(int width, int height)
+{
+}

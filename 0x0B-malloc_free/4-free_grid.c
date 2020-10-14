c9ae82ef29ce46b0a4e33061095292656d4f1622
Fares84 <[2528@holbertonschool.com]>
2020-10-14 21:54:31 +0000

    freez

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,9 +2,16 @@
 #include <stdlib.h>
 
 /**
- *void free_grid(int **grid, int height) - function that frees a 2 dimensional
- * grid previously created
- * @grid: grid to free
- * @height: height of grid
- * Return: void
+ *free_grid - function that frees a 2 dimensional
+ *previously created
+ *@grid: grid to free
+ *@height: height of grid
+ *Return: void
  **/
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	free(grid[i]);
+}

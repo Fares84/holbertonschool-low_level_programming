c60a81075be70d4dd529a1efc23e63e9c96fd8c2
Fares84 <[2528@holbertonschool.com]>
2020-10-14 09:35:28 +0000

    free free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -4,9 +4,34 @@
 /**
 ***alloc_grid - function function that returns a pointer to a newly allocated
 *@width: width of array
-* @height: height of array
-* Return: a double pointer to the 2D array
+*@height: height of array
+*Return: a double pointer to the 2D array
 **/
 int **alloc_grid(int width, int height)
 {
+	int i, j;
+	int **array;
+
+		if (width <= 0 || height <= 0)
+			return (NULL);
+		array = malloc(sizeof(int *) * height);
+		if (array == NULL)
+			return (NULL);
+		for (i = 0; i < height; i++)
+		{
+			array[i] = malloc(sizeof(int) * width);
+			if (array[i] == NULL)
+			{
+				for (j = 0; j < i; j++)
+					free(array[j]);
+				free(array);
+				return (NULL);
+			}
+
+			for (j = 0; j < width; j++)
+			{
+				array[i][j] = 0;
+			}
+		}
+		return (array);
 }

2f35dd035d87017e7852f189341dac7d0acea0e4
Fares84 <[2528@holbertonschool.com]>
2020-10-14 07:25:34 +0000

    string up

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -7,7 +7,20 @@
  *@str: string to copy
  *Return: char
  **/
-char *str_concat(char *s1, char *s2)
+char *_strdup(char *str)
 {
-	
+
+	int size;
+	char *p;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (size = 0; str[size] != '\0'; size++)
+	p = malloc((size + 1) * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	for (size = 0; str[size] != '\0'; size++)
+		p[size] = str[size];
+	return (p);
 }

a55e60fbc6be53b533a2689b931b58f61d1f1a3b
Fares84 <[2528@holbertonschool.com]>
2020-10-16 09:23:30 +0000

    concat plus const

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,16 +13,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int length1;
 	unsigned int length2;
+	unsigned int i;
 	char *string;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (length1 = 0; s1[length1] != '0'; length1++)
+	for (length1 = 0; s1[length1] != '\0'; length1++)
 	{
 	}
-	for (length2 = 0; s2[length2] != '0'; length2++)
+	for (length2 = 0; s2[length2] != '\0'; length2++)
 	{
 	}
 
@@ -32,10 +33,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if  (string == NULL)
 		return (NULL);
-	for (length1 = 0; s1[length1] != '0'; length1++)
-		string[length1] = s1[length1];
-	for (length2 = 0; s2[length2] != '0'; length2++)
-		string[length2] = s2[length2];
-	string[length1] = s2[length2];
+	for (i = 0; i < length1; i++)
+		string[i] = s1[i];
+	for (i = 0; i < n; i++)
+		string[length1 + i] = s2[i];
+	string[length1 + n] = '\0';
 	return (string);
 }

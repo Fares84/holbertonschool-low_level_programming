8073db184fec5d8477aea10da2e1c7108d5b9e2a
Fares84 <[2528@holbertonschool.com]>
2020-10-19 21:52:33 +0000

    create new

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ *print_dog -  function that prints a struct dog
+ *@name: dog's name
+ *@age: dog's age
+ *@owner: dog's owner
+ *Return: pointer to new dog
+ **/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+}

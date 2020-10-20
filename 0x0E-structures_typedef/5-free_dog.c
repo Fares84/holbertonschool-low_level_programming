fd27ef7ce5cc300b43ad4bc09a96e8a875e3c406
Fares84 <[2528@holbertonschool.com]>
2020-10-20 13:46:18 +0000

    free dog 4

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,11 +2,16 @@
 #include <stdlib.h>
 
 /**
- *print_dog -  function that prints a struct dog
+ *free_dog -  function that prints a struct dog
  *@d: pointer to dog to free
  *Return: void
  **/
 void free_dog(dog_t *d)
 {
-	
+	if (d)
+	{
+		free((*d).name);
+		free((*d).owner);
+		free(d);
+	}
 }

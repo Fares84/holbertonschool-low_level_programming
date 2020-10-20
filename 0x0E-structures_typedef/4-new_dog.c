db42959e611bf59bff23346fd807f050034b9501
Fares84 <[2528@holbertonschool.com]>
2020-10-20 13:30:30 +0000

    crazy new

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,8 +1,9 @@
 #include "dog.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- *print_dog -  function that prints a struct dog
+ *new_dog -  function that prints a struct dog
  *@name: dog's name
  *@age: dog's age
  *@owner: dog's owner
@@ -10,5 +11,39 @@
  **/
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int newname;
+	int newowner;
 
+
+	dog_t *new_dog = malloc(sizeof(dog_t));
+
+	if (new_dog == NULL)
+		return (NULL);
+
+	for (newname = 0; name[newname]; newname++)
+	{
+	}
+	for (newowner = 0; owner[newowner]; newowner++)
+	{
+	}
+	(*new_dog).name = malloc((newname + 1) * sizeof(char));
+	(*new_dog).owner = malloc((newowner + 1) * sizeof(char));
+
+	if ((*new_dog).name == NULL || (*new_dog).owner == NULL)
+	{
+		free((*new_dog).name);
+		free((*new_dog).owner);
+		free(new_dog);
+		return (NULL);
+	}
+
+		for (newname = 0; name[newname]; newname++)
+			(*new_dog).name[newname] = name[newname];
+		for (newowner = 0; owner[newowner]; newowner++)
+			(*new_dog).owner[newowner] = owner[newowner];
+
+		(*new_dog).name[newname] = 0;
+		(*new_dog).owner[newowner] = 0;
+		(*new_dog).age = age;
+		return (new_dog);
 }

ab9e7a3acde3a131a4b8c23b0107ebeb31f32a3c
Fares84 <[2528@holbertonschool.com]>
2020-10-21 21:54:27 +0000

    search for int

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+
+/**
+ *int_index - function that searches for an integer
+ *@array: array pointer to int
+ *@size: number of element in the array
+ *cmp: pointer to a function that receives an int
+ *Return: int
+ **/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int el;
+	int index;
+
+	if (array != NULL && cmp != NULL)
+	{
+		if (size <= 0)
+			return (-1);
+
+		for (el = 0; el < size; el++)
+		{
+			index = cmp(array[elements]);
+			if(index > 0)
+				return (el);
+		}
+	}
+	return (-1);
+
+
+}

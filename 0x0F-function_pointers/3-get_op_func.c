f4f92fb7383abf0e7c85e8856f1388483521303f
Fares84 <[2528@holbertonschool.com]>
2020-10-22 23:35:36 +0000

    fix add strlen

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "3-calc.h"
+#include <string.h>
 
 /**
  *get_op_func - unction that selects the correct function to perform the
@@ -20,10 +21,10 @@ int (*get_op_func(char *s))(int, int)
 
 	int i = 0;
 
-	while (ops[i].op != NULL)
+	while (ops[i].op)
 	{
-		if (*(ops[i].op) == *s && strlen(s) == 1)
-			return (ops[i].f);
+		if (*(ops[i]).op == *s && strlen(s) == 1)
+			return (*(ops[i]).f);
 		i++;
 	}
 

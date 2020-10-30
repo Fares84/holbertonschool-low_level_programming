eb3e267ecc3647aab2df4b64d77c73978d38a308
Fares84 <[2528@holbertonschool.com]>
2020-10-30 21:51:37 +0000

    free head list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,11 +2,16 @@
 
 /**
  *free_list - function that frees a list_t list
- *@head:
- *Return:
+ *@head: pointer to the head of the list tp free
+ *Return: void
  */
 void free_list(list_t *head)
 {
-
-
+	if (head)
+	{
+		free_list(head->next);
+		if (head->str)
+			free(head->str);
+		free(head);
+	}
 }

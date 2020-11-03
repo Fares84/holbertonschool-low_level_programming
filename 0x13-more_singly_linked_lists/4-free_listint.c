5c3175d942c85605433d9019f84d07291ca4b502
Fares84 <fares.sassi2015@gmail.com>
2020-11-03 14:30:58 +0000

    free exo0

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+*free_listint -  function that frees a listint_t list
+*@head: pointer to the head of the list
+*Return: void
+*/
+void free_listint(listint_t *head)
+{
+	if (head)
+	{
+		free_listint(head->next);
+		free(head);
+	}
+}

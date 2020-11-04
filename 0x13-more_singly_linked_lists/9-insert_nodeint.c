61be5d247c3245e4cf4b306f987278999096bd0f
Fares84 <fares.sassi2015@gmail.com>
2020-11-04 16:37:35 +0000

    second update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,7 +10,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *insert_node, *get_node;
-	unsigned int i;
+	unsigned int i = 0;
 
 	get_node = *head;
 

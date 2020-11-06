761be4ef4f068f6016c9b605f2eafb1a56157088
Fares84 <fares.sassi2015@gmail.com>
2020-11-06 22:49:39 +0000

    little big egg

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *get_endianness -  function that checks the endianness
+ *Return: 1 if little endianness and 0 if big endianness
+ */
+int get_endianness(void)
+{
+	int i;
+	char *c =(char *)&i;
+
+	if (*c)
+		return (1);
+	return (0);
+}

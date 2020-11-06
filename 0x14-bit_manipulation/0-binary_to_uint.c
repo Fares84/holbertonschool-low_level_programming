0cee15c71ca41579a88abac92cede07b709f4349
Fares84 <fares.sassi2015@gmail.com>
2020-11-06 12:29:13 +0000

    fix file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,13 +9,13 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i, conv = 0, bin = 1;
+	int i, bin = 1, conv = 0;
 
 	if (!b)
 		return (0);
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != '1' && b[i] != 0)
+		if (b[i] != '1' && b[i] != '0')
 			return (0);
 	}
 	i--;

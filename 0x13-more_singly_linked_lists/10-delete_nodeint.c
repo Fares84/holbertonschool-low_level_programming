03277c4b9e10da9dac50b6d4998a1f027ea6b748
Fares84 <fares.sassi2015@gmail.com>
2020-11-08 20:38:00 +0000

    fix file

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -21,7 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(trash);
 		return (1);
 	}
-	if (i < index - 1)
+	if (trash != NULL && i < index - 1)
 	{
 		trash = trash->next;
 		i++;

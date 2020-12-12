a8ec4de209a522a5aec4a96ab04a44f90704fc06
Fares84 <fares.sassi2015@gmail.com>
2020-12-12 23:25:23 +0000

    print DLL

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- *print_dlisint -  function that prints all the elements of a dlistint_t list
+ *print_dlistint - function that prints all the elements of a dlistint_t list
  *@h: head node
  *Return: number of nodes printed
  */
@@ -15,5 +15,5 @@ size_t print_dlistint(const dlistint_t *h)
 		h = h->next;
 		n++;
 	}
-	return(n);
+	return (n);
 }

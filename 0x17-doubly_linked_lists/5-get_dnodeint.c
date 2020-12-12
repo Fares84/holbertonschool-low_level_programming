6cc0b4d0cb8d61f117e7e33fcfbf609ba8808c5d
Fares84 <fares.sassi2015@gmail.com>
2020-12-12 23:05:15 +0000

    add node at index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+*get_dnodeint_at_index - function that returns node at given index
+*@head: head node
+*@index: index
+*Return: node
+*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int idx = 0;
+	dlistint_t *new_node;
+
+	new_node = head;
+	if (new_node == NULL)
+		return (NULL);
+	while (new_node)
+	{
+		if (idx == index)
+			return (new_node);
+	new_node = new_node->next;
+	}
+	return (new_node);
+}

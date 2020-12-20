8df615970af3778e478dff7ee9ace9c49440e872
Fares84 <fares.sassi2015@gmail.com>
2020-12-20 21:51:28 +0000

    fixed file

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,47 +10,28 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i;
-	dlistint_t *node = *h, *new_node = NULL, *save_node = NULL;
+	dlistint_t *new_node, *save_node;
 
-	new_node = malloc(sizeof(dlistint_t));
-	if(new_node == NULL)
-		return (NULL);
-	new_node->n = n;
-	new_node->next = NULL;
-	new_node->prev = NULL;
-
-
+	save_node = *h;
 	if (idx == 0)
+		return (add_dnodeint(h, n));
+	while (i != idx -1)
 	{
-		if (!node)
-			*h = new_node;
-		else
-		{
-			new_node->next = *h;
-			(*h)->prev = new_node;
-			*h = new_node;
-		}
-		return (new_node);
+		if (!save_node)
+			return (NULL);
+		save_node = save_node->next;
+		i++;
 	}
-
-	for(i = 0; i < idx-1; i++)
-	{
-		node = node->next;
-	}
+	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-	if (node->next)
-	{
-		save_node = node->next;
-		node->next = new_node;
-		new_node->prev = node;
-		new_node->next = save_node;
-		save_node->prev = new_node;
-	}
-	else
-	{
-		node->next = new_node;
-		new_node->prev = node;
-	}
+	if (save_node->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new_node->n = n;
+	save_node->next->prev = new_node;
+	new_node->prev = save_node;
+	new_node->next = save_node->next;
+	save_node->next = new_node;
 	return (new_node);
 }

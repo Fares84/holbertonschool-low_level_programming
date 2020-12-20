06febf2b3ef29c30c9172c054a03f44abb5492ac
Fares84 <fares.sassi2015@gmail.com>
2020-12-20 20:26:21 +0000

    delete at index

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ *delete_dnodeint_at_index - delete node at given index DLL
+ *@head: head node
+ *@index: index
+ *Return: 1 on success, -1 on failure
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t  *save_next, *save_prev;
+
+	save_prev = *head;
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		if (!save_prev)
+			return (-1);
+		else
+			(*head)->prev = NULL;
+		return (1);
+	}
+
+	while (save_prev)
+	{
+		if (i == index)
+		{
+			save_next = save_prev->next;
+			if (save_next)
+			{
+				save_prev->next = save_next->next;
+				if (save_next->next)
+					save_next->next->prev = save_prev;
+				return (1);
+			}
+		}
+		save_prev = save_prev->next;
+		i++;
+	}
+	return (-1);
+}

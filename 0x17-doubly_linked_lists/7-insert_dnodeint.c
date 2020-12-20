13fe6c64291e37381baf4965ad0efb4244a01af0
Fares84 <fares.sassi2015@gmail.com>
2020-12-20 21:53:41 +0000

    fixed file

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	save_node = *h;
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	while (i != idx -1)
+	while (i != idx - 1)
 	{
 		if (!save_node)
 			return (NULL);
